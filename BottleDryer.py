eff9b7340216c2c84589fe2be8ad0554aa43fbca
Thomas <git@thomas.klierlinge.de>
2025-04-11 23:14:38 +0200

    Fix script

--- BottleDryer.py
+++ BottleDryer.py
@@ -5,10 +5,11 @@
 import os
 
 from itertools import product
+from numbers import Number
 from subprocess import Popen
 from typing import List
 
-variables = {'stand_count': range(1, 10), 'fan_diameter': [40, 80], 'base_type': ['"SEPARATE"', '"COMMON"']}
+variables = {'stand_count': range(1, 10), 'fan_diameter': [40, 80], 'base_type': ['SEPARATE', 'COMMON']}
 
 permutations = []
 keys = list(variables.keys())
@@ -18,14 +19,16 @@ for value_tuple in product(*values):
 
 calls = []
 for params in permutations:
-    out_file = f'BottleDryer_{params['stand_count']}Arms_{params['fan_diameter']}mmFan_{params['base_type'][0]}{params['base_type'][1:].lower()}Base'
+    out_file = f'BottleDryer_{params["stand_count"]}Arms_{params["fan_diameter"]}mmFan_{params["base_type"][0]}{params["base_type"][1:].lower()}Base'
     call = ['openscad', '-o', f'{out_file}.stl', '-o', f'{out_file}.png', '-D', '$fn=200', '--imgsize=1000,1000']
     for key, value in params.items():
+        if not isinstance(value, Number):
+            value = f'"{value}"'
         call.extend(['-D', f'{key}={value}'])
     call.append('BottleDryer.scad')
     calls.append(call)
 
-cpu_count = 10
+cpu_count = 2
 processes = set()
 
 for call in calls:
@@ -35,4 +38,6 @@ for call in calls:
         os.wait()
         processes.difference_update([
             p for p in processes if p.poll() is not None])
+for p in processes:
+    p.wait()
 
