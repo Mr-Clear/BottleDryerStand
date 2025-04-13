d762c89c872c8a912cc3b3082db6d6770ed4e43d
Thomas <git@thomas.klierlinge.de>
2025-04-13 10:01:04 +0200

    Rename batch create script

--- BatchCreate.py
+++ BatchCreate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" Bulk create a set otf bottle dryer STL files """
+""" Bulk create a set of bottle dryer STL files """
 
 import os
 
@@ -9,7 +9,12 @@ from numbers import Number
 from subprocess import Popen
 from typing import List
 
-variables = {'stand_count': range(1, 10), 'fan_diameter': [40, 80], 'base_type': ['SEPARATE', 'COMMON']}
+quality = 20
+image_size = (100, 100)
+variables = {'stand_count': range(1, 11, 1),
+             'fan_diameter': [40, 50, 60, 70, 80, 92, 120, 140, 200, 220], 
+             'base_type': ['SEPARATE', 'COMMON']}
+cpu_count = 8
 
 permutations = []
 keys = list(variables.keys())
@@ -20,7 +25,8 @@ for value_tuple in product(*values):
 calls = []
 for params in permutations:
     out_file = f'BottleDryer_{params["stand_count"]}Arms_{params["fan_diameter"]}mmFan_{params["base_type"][0]}{params["base_type"][1:].lower()}Base'
-    call = ['openscad', '-o', f'{out_file}.stl', '-o', f'{out_file}.png', '-D', '$fn=200', '--imgsize=1000,1000']
+    call = ['openscad', '-o', f'{out_file}.stl', '-o', f'{out_file}.png', '-D', '$fn={quality}', '--imgsize={image_size[0]},{image_size[1]}', '-D', f'$quality={quality}', '-D', f'$imgsize={image_size[0]},{image_size[1]}']
+
     for key, value in params.items():
         if not isinstance(value, Number):
             value = f'"{value}"'
@@ -28,12 +34,11 @@ for params in permutations:
     call.append('BottleDryer.scad')
     calls.append(call)
 
-cpu_count = 2
 processes = set()
 
 for call in calls:
     print(f'Running: {" ".join(call)}')
-    processes.add(Popen(call))
+    processes.add(Popen(call, stdout=None))
     if len(processes) >= cpu_count:
         os.wait()
         processes.difference_update([
